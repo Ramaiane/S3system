ef03565e4162778caf39e9871c11ab6f25560fbf
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2013-06-02 17:35:13 -0300

    Controle de Usuários  - Teste Conexão

---
+++ S3.System/controle/testeConexao.php
@@ -0,0 +1,13 @@
+<?php
+// Create connection
+$con=mysqli_connect("localhost","s3system","pirulito","s3system");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit;
+  }
+  
+  echo "parece estar funcionando!";
+?>
