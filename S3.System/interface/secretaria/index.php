13a8901d7f6b79f7fc510ec324f3e7c473623da5
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2013-06-05 23:37:47 -0300

    Interface - Index da Secretaria e Form para Cadastro de Professores

--- S3.System/interface/secretaria/index.php
+++ S3.System/interface/secretaria/index.php
@@ -1,29 +1,32 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>S3 System</title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
-        <link rel="stylesheet" href="../css/style.css" type="text/css">
-        <link rel="stylesheet" href="../css/style.css" type="text/css"/>
+<?php
+include_once("../../controle/AcessoRestrito.php");
+$objAcesso = new AcessoRestrito;
+$objAcesso->iniciaSessao();
+$objAcesso->set('usuario_tipo', 2);
+$objAcesso->verificaAcesso();
+?>
 
-
-    </head>
-   <!--  <html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
 <title>S3 System</title>
-<link rel="stylesheet" type="text/css" href="style.css" />
-</head> -->
-    <body>
+<link rel="stylesheet" type="text/css" href="../style.css" />
+</head>
+<body>
 
 <div id="main_container">
+    <!--
+    <div id="header">
+    	<div class="logo"><img src="" border="0" alt="" title="" /></div>       
+    </div>
+    -->
     
         <div class="menu">
         	<ul>                                                                         
         		<li class="selected"><a href="#">Home</a></li>
-                <li><a href="#">Editais</a></li>
-                <li><a href="#">Informações</a></li>
+                <li><a href="#">Parceiros</a></li>
+                <li><a href="#">Documenta&ccedil;&atilde;o</a></li>
                 
         	</ul>
         </div>
@@ -31,95 +34,60 @@
     <div class="center_content">
     
      	<div class="center_left">
-        	<div class="title_welcome"><span class="red">S3 System</span> Seleção de Alunos Especiais</div>
+            <div class="title_welcome"><span class="red">S3 System</span> P&aacute;gina do Candidato.</div>
             <div class="welcome_box">
             <p class="welcome">
-<span class="orange">Cadastro Secretaria</span><br />
-Por favor, preencha todos os campos abaixo.
+<span class="orange">Seja bem vindo candidato <?php echo $_SESSION['usuario_login']; ?> </span><br />
+Este Sistema est&aacute; em desenvolvimento. Favor aguardar futuras releases. Por enquanto s&oacute; pode fazer logoff ou criar uma conta de professor. 
 			</p>
-                <form name="cadastro" method="post" action="controleSecretaria.php">
-             <table>
-                 <tr>
-                     <td>Sigla: </td><td><input type="text" name="siglatxt"/></td>
-                 </tr>
-                 <tr>
-                     <td>Nome Progrma: </td><td><input type="text" name="nomeProgramatxt"/></td>
-                 </tr>
-                 <tr>
-                     <td>Responsável: </td><td><input type="text" name="rsponsaveltxt"/></td>
-                 </tr>
-                  <tr>
-                      <td>Endereço Logo: </td><td><input type="text" name="enderecoLogotxt"/></td>
-                 </tr>
-                 <tr>
-                      <td>Telefone Contato: </td><td><input type="text" name="telefoneContatotxt"/></td>
-                 </tr>
-                 <tr>
-                      <td>Email Contato: </td><td><input type="text" name="emailContatotxt"/></td>
-                 </tr>
-                 <tr>
-                      <td>Data Registro: </td><td><input type="text" name="dataRegistrotxt"/></td>
-                 </tr>
-                 <tr>
-                     <td><br /><input type="submit" value="Cadastrar"/></td><td></td>
-                 </tr>
-             </table>
-        </form>
+</br></br></br> 
+            <a href="../cadastro/cadastroProfessor.php" class="read_more">Cadastrar</a> 
+            <a href="#" class="read_more">Logoff</a>          
             </div>
          
-         <!-- <div class="features">   
-            <div class="title">Main features</div>
-            
-                    <ul class="list">
-                    <li><span>1</span><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</a></li>
-                    <li><span>2</span><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</a></li>
-                    <li><span>3</span><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</a></li>
-                    <li><span>4</span><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</a></li>
-                    </ul> 
-         </div>  -->
          
-        </div>
+         
+         
+        <!-- <div class="features">   
+            <div class="title">Latest News</div>
+                <div class="news_box">
+                    <div class="news_icon"></div>
+                    <div class="news_content">
+                    “Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    </div>   
+                </div>
+                <div class="news_box">
+                    <div class="news_icon"></div>
+                    <div class="news_content">
+                    “Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    </div>   
+                </div>       
+
+         </div> 
+         
+         -->
+           
+            
+            
+        </div> 
         
-        <div class="center_right">
-                
-                        <div class="text_box">
-                        <div class="title">Login</div>
-                            <div class="login_form_row">
-                            <label class="login_label">Login:</label><input type="text" name="name" class="login_input" />
-                            </div>
-                            
-                            <div class="login_form_row">
-                            <label class="login_label">Senha:</label><input type="password" name="pass" class="login_input" />
-                            </div>                                     
-                            <!--<input type="image" src="images/login.gif" class="login" />   -->                           
-                        
-                        </div>
-                        
-                          <div class="testimonials">
-                        	<div class="title"></div>
-                            <div class="text_box">
-                             <!--<p class="testimonial">
-                             “Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam”<br />
-                             <strong>Edward Caloryd</strong>
-                             
-                             </p>-->
-                            
-                            </div>                                   
-                        
-        
-        </div>  
         
         <div class="clear"></div> 
     
     </div>    
- 
+
+    
     <div id="footer">                                              
         <div class="left_footer"><a href="#">S3 System, 2013</a> <a href="#">Reporte Um Erro</a></div>
-        <div class="right_footer"><a href=""  target="_blank"><img src="" border="0" alt="" title="" /></a>
+        <div class="right_footer"><a href="http://csstemplatesmarket.com"  target="_blank"><img src="images/csstemplatesmarket.gif" border="0" alt="" title="" /></a>
         </div>   
     
-    </div>    
+    </div>
+    
+    
     
 </div>
+<!-- end of main_container -->
+
 </body>
 </html>
