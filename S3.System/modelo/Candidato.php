0858360dd441079063be56295bcb954fc5dd1558
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2013-06-25 22:07:53 -0300

    [Modelo] Criação da Classe Candidato

---
+++ S3.System/modelo/Candidato.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Candidato
+ *
+ * @author Fillipe
+ */
+class Candidato {
+    private $login;
+    private $senha;
+    private $cpf;
+    private $email;
+    private $telefone;
+    private $conclusaoGraduacao;
+    
+    function set($prop, $value){
+         $this->$prop = $value;
+    }
+}
+
+?>
