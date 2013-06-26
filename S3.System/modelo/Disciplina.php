c87f0c7df08edc3586a219aa2b8cab204affc6f8
tainarareis <tainarareis@192.168.1.3>
2013-06-25 22:26:35 -0300

    Classe Disciplina na pasta Modelo FINALIZADA

--- S3.System/modelo/Disciplina.php
+++ S3.System/modelo/Disciplina.php
@@ -26,6 +26,8 @@ class Disciplina {
     private $numeroCreditos;
     private $ementa;
     private $inscricao;
+    private $prop;
+    private $value;
     
     //método construtor
     public function __construct($orgao, $codigo, $denominacao, $nivel, $vigencia, $preRequisitos, 
@@ -47,94 +49,59 @@ class Disciplina {
     }
     
     // setters e getters
-    public function setOrgao ($orgao) {
-        $this-> orgao= $orgao;     
+    
+    public function set($prop, $value){
+        $this->$prop = $value;
     }
+    
     public function getOrgao () {
         return $this->orgao;
     }
     
-    public function setCodigo ($codigo) {
-        $this-> codigo= $codigo;
-    }
     public function getCodigo () {
         return $this->codigo;
     }
     
-    public function setDenominacao ($denominacao) {
-        $this-> denominacao= $denominacao;
-    }
     public function getDenominacao () {
         return $this->denominacao;
     }
     
-    public function setNivel ($nivel) {
-        $this-> nivel= $nivel;
-    }
     public function getNivel () {
         return $this->nivel;
     }
     
-    public function setVigencia ($vigencia) {
-        $this-> vigencia= $vigencia;
-    }
     public function getVigencia () {
         return $this->vigencia;
     }
     
-    public function setPreRequisitos ($preRequisitos) {
-        $this-> preRequisitos= $preRequisitos;
-    }
     public function getPreRequisitos () {
         return $this->preRequisitos;
     }
     
-    public function setTurma ($turma) {
-        $this-> turma= $turma;
-    }
     public function getTurma () {
         return $this->turma;
     }
     
-    public function setNumeroVagas ($numeroVagas) {
-        $this-> numeroVagas= $numeroVagas;
-    }
     public function getNumeroVagas () {
         return $this->numeroVagas;
     }
     
-    public function setTurno ($turno) {
-        $this-> turno= $turno;
-    }
     public function getTurno () {
         return $this->turno;
     }
     
-    public function setNumeroCreditos ($numeroCreditos) {
-        $this-> numeroCreditos= $numeroCreditos;
-    }
     public function getNumeroCreditos () {
         return $this->numeroCreditos;
     }
    
-    
-    public function setHorario ($horario) {
-        $this-> horario= $horario;
-    }
     public function getHorario () {
         return $this->horario;
     }
 
-    public function setEmenta ($ementa) {
-        $this-> ementa= $ementa;
-    }
     public function getEmenta () {
         return $this->ementa;
     }
 
-    public function setInscricao ($inscricao) {
-        $this-> inscricao= $inscricao;
-    }
     public function getInscricao () {
         return $this->inscricao;
     }
