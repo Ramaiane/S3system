793909f51680fea52ab8884574ce6a255e50f0d3
Camila Marinho <kmila2012@hotmail.com>
2013-07-03 18:01:51 -0300

    [modelo]modificação em professor

--- S3.System/modelo/Professor.php
+++ S3.System/modelo/Professor.php
@@ -3,10 +3,26 @@ include_once 'Usuario.php';
 
 class Professor extends Usuario{
     
+    private $idProfessor;
+    private $idUsuario;
+    private $idSecretaria;
     private $professor_nome;
     private $professor_email;
-    private $professor_contatoTelefone;   
+    private $professor_contatoTelefone;  
+    private $prop;
+    private $value;
     
+//    public $tipo = 2;
+    
+    public function __construct($idProfessor, $idUsuario, $idSecretaria,
+            $professor_nome, $professor_email, $professor_contatoTelefone){
+        $this-> orgao= $idProfessor; 
+        $this-> codigo= $idUsuario;
+        $this-> denominacao= $idSecretaria;
+        $this-> nivel= $professor_nome;
+        $this-> vigencia= $professor_email;
+        $this-> preRequisitos= $professor_contatoTelefone;        
+    }
     
        function set($prop, $value){
          $this->$prop = $value;
