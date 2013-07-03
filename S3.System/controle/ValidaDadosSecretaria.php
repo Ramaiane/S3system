d400f786472f5ca000317eef94ef3fdcb5fbf22f
anacarolinals <ana_karolopes@hotmail.com>
2013-07-03 15:27:29 -0300

    [modelo] adição validaDadosSecretaria

---
+++ S3.System/controle/ValidaDadosSecretaria.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ValidaDadosSecretaria
+ *
+ * @author Carol
+ */
+class ValidaDadosSecretaria {
+    
+   function checaCampoEmBranco($dado){
+        if (!isset($dado) || ($dado=="")){
+            header( "refresh:3;url=../interface/cadastro/cadastroSecretaria.php" ); 
+            echo "Informe todos os campos para prosseguir com cadastro!";
+            exit;
+        }
+    }
+    function validaEmail($mail){
+	if(!preg_match("/^([[:alnum:]_.-]){3,}@([[:lower:][:digit:]_.-]{3,})(\.[[:lower:]]{2,3})(\.[[:lower:]]{2})?$/", $mail)) {
+            header( "refresh:3;url=../interface/cadastro/cadastroSecretaria.php" ); 
+            echo "Informe um email Valido para Prosseguir com o cadastro";
+            exit;
+	}
+    }
+    
+    
+    function verificaSenha($senha1, $senha2){
+        if(strlen($senha1) < 6){
+            header( "refresh:3;url=../interface/cadastro/cadastroSecretaria.php" ); 
+            echo "Senha muito curta. Por favor informe uma senha de no minimo 6 caracteres";
+            exit;
+        }
+        if($senha1 != $senha2){
+            header( "refresh:3;url=../interface/cadastro/cadastroSecretaria.php" ); 
+            echo "As senhas informadas não batem";
+            exit;
+        }
+    }
+    function campoNumerico($dadoNumerico){
+        if(!is_numeric($dadoNumerico)) {
+	    header( "refresh:3;url=../interface/cadastro/cadastroSecretaria.php" ); 
+            echo "Favor, atentar para campo numerico";
+            exit;
+	}        
+    }
+}
+
+?>
