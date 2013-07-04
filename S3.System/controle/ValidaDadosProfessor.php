f2d0a9356f3221b4844b9edf5ad6f6481efe16fc
Camila Marinho <kmila2012@hotmail.com>
2013-07-04 00:32:48 -0300

    [controle] validação de dados professor

---
+++ S3.System/controle/ValidaDadosProfessor.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ValidaDadosProfessor {
+    private $login;
+    private $nome;
+    private $senha1;
+    private $senha2;
+    private $email;
+    private $telefone;
+            
+    function set($prop, $value){
+         $this->$prop = $value;
+    }
+    
+    
+    
+}
+?>
