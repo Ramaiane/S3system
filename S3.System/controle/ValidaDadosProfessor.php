a1a0905ac281c3082aa3a695bd8107b2d68e6f7d
anacarolinals <ana_karolopes@hotmail.com>
2013-07-10 22:05:30 -0300

    [DAO] atualizacao validaDadosProfessor

--- S3.System/controle/ValidaDadosProfessor.php
+++ S3.System/controle/ValidaDadosProfessor.php
@@ -6,18 +6,43 @@
  */
 
 class ValidaDadosProfessor {
-    private $login;
-    private $nome;
-    private $senha1;
-    private $senha2;
-    private $email;
-    private $telefone;
-            
-    function set($prop, $value){
-         $this->$prop = $value;
+    function checaCampoEmBranco($dado){
+        if (!isset($dado) || ($dado=="")){
+            header( "refresh:3;url=../interface/cadastro/cadastroProfessor.php" ); 
+            echo "Informe todos os campos para prosseguir com cadastro!";
+            exit;
+        }
+    }
+    function validaEmail($mail){
+	if(!preg_match("/^([[:alnum:]_.-]){3,}@([[:lower:][:digit:]_.-]{3,})(\.[[:lower:]]{2,3})(\.[[:lower:]]{2})?$/", $mail)) {
+            header( "refresh:3;url=../interface/cadastro/cadastroProfessor.php" ); 
+            echo "Informe um email Valido para Prosseguir com o cadastro";
+            exit;
+	}
     }
     
     
+    function verificaSenha($senha1, $senha2){
+        if(strlen($senha1) < 6){
+            header( "refresh:3;url=../interface/cadastro/cadastroProfessor.php" ); 
+            echo "Senha muito curta. Por favor informe uma senha de no minimo 6 caracteres";
+            exit;
+        }
+        if($senha1 != $senha2){
+            header( "refresh:3;url=../interface/cadastro/cadastroProfessor.php" ); 
+            echo "As senhas informadas não batem";
+            exit;
+        }
+    }
+    function campoNumerico($dadoNumerico){
+        if(!is_numeric($dadoNumerico)) {
+	    header( "refresh:3;url=../interface/cadastro/cadastroProfessor.php" ); 
+            echo "Favor, atentar para campo numerico";
+            exit;
+	}        
+    }
     
 }
+
+
 ?>
